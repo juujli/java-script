f6f8115e88c12b96bf242466cb4d33697855a015
Júlia Marques <juliasantosmarques6@gmail.com>
2022-11-22 23:04:52 -0300

    salve

---
+++ salario.js
@@ -0,0 +1,7 @@
+let horas = prompt("Digite a quantidade de horas trabalhadas");
+
+let valorHora = 86;
+
+let valorTotal = horas * valorHora;
+
+window.document.write(`Esse é o seu sálario: ${valorTotal}`);
