71917f8971d4c36af35b7d3cffe0b1167d8723df
Júlia Marques <juliasantosmarques6@gmail.com>
2022-11-22 22:37:04 -0300

    first commit

---
+++ cliente.js
@@ -0,0 +1,17 @@
+let nomeCliente1 = prompt("Digite o nome do primeiro cliente");
+let emailCliente1 = prompt("Digite o email do primeiro cliente");
+
+let nomeCliente2 = prompt("Digite o nome do segundo cliente");
+let emailCliente2 = prompt("Digite o email do segundo cliente");
+
+let nomeCliente3 = prompt("Digite o nome do terceiro cliente");
+let emailCliente3 = prompt("Digite o email do terceiro cliente");
+
+window.document.write(`Nome Cliente 1: ${nomeCliente1}  </br>`);
+window.document.write(`Email Cliente 1: ${emailCliente1}  </br>`);
+
+window.document.write(`Nome Cliente 2: ${nomeCliente2}  </br>`);
+window.document.write(`Email Cliente 2: ${emailCliente2}  </br>`);
+
+window.document.write(`Nome Cliente 3: ${nomeCliente3}  </br>`);
+window.document.write(`Email Cliente 3: ${emailCliente3}  </br>`);
